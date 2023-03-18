71c6d9cd3e8902402ec8c05d3f9f7f6ce5075441
Young-ook Kim <youngook.kim@gmail.com>
2023-03-18 14:02:24 +0900

    chore(chatapp): grooming message component

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,198 +1,22 @@
 // ui
 import React, { useState, useEffect } from "react";
 import {
-  Button, Form, Modal, Textarea,
-  Box, Container, Grid, Header, SpaceBetween
+  Box,
+  Button,
+  Form,
+  Modal,
+  SpaceBetween,
+  Textarea
 } from "@cloudscape-design/components";
 
-//api
+//apis
 import { API, graphqlOperation } from 'aws-amplify'
 import { listMessages } from '../graphql/queries'
 import { createMessage, updateMessage, deleteMessage } from '../graphql/mutations'
 import { onCreateMessage, onUpdateMessage, onDeleteMessage } from '../graphql/subscriptions';
 
-// util
+// utils
 import moment from "moment";
-
-function createMessageApi(post, channelId) {
-  try {
-    API.graphql(graphqlOperation(createMessage, {
-      input: { content: post, channelId: channelId }
-    }));
-  }
-  catch (e) {
-    console.log({e});
-  }
-}
-
-function editMessageApi(messageId, messageVersion, post) {
-  try {
-    API.graphql(graphqlOperation(updateMessage, {
-      input: { id: messageId, content: post, _version: messageVersion }
-    }));
-  }
-  catch (e) {
-    console.log({e});
-  }
-}
-
-function deleteMessageApi(messageId, messageVersion) {
-  try {
-    API.graphql(graphqlOperation(deleteMessage, {
-      input: { id: messageId, _version: messageVersion }
-    }));
-  }
-  catch (e) {
-    console.log({e});
-  }
-}
-
-function NewLineToBr({children = ""}) {
-  return children.split('\n').reduce(function (arr, line) {
-    return arr.concat(line, <br />);
-  },[]);
-}
-
-const NoMessage = () => {
-  return (
-    <SpaceBetween size="l">
-      <Box
-        margin={{ vertical: 'xs' }}
-        fontSize="heading-s"
-        textAlign="center"
-        color="inherit"
-      >
-        Please leave a message
-      </Box>
-    </SpaceBetween>
-  );
-}
-
-const Message = ({
-  message,
-  activeMessage,
-  setActiveMessage,
-}) => {
-  const isEditing = activeMessage && activeMessage.type === "edit" && activeMessage.id === message.id
-  const [confirmVisible, setConfirmVisible] = useState(false);
-
-  const deleteHandler = () => {
-    deleteMessageApi(message.id, message._version);
-    setConfirmVisible(false);
-  }
-
-  return (
-    isEditing ?
-    <MessageForm
-      initText={message.content}
-      channelId={message.channelId}
-      messageId={message.id}
-      messageVersion={message._version}
-      activeMessage={activeMessage}
-      setActiveMessage={setActiveMessage}
-     /> :
-    <Container
-      header={
-        <Header
-          variant="h4"
-          description={ moment(message.updatedAt).fromNow() }
-          actions={
-            <SpaceBetween direction="horizontal" size="xxs">
-              <Button iconName="contact" variant="icon" />
-              <Button iconName="edit" variant="icon" onClick={() => setActiveMessage({ id: message.id, type: "edit"})} />
-              <Button iconName="remove" variant="icon" onClick={() => setConfirmVisible(true)} />
-            </SpaceBetween>
-          }
-        >
-          User name
-        </Header>
-      }
-    >
-      <Modal
-        onDismiss={() => setConfirmVisible(false)}
-        visible={confirmVisible}
-        closeAriaLabel="Close modal"
-        size="small"
-        footer={
-          <Box float="right">
-            <SpaceBetween direction="horizontal" size="xs">
-              <Button variant="link" onClick={() => setConfirmVisible(false)}>Cancel</Button>
-              <Button variant="primary" onClick={deleteHandler}>OK</Button>
-            </SpaceBetween>
-          </Box>
-        }
-      >
-        Are you sure to delete the message?
-      </Modal>
-      <NewLineToBr>{message.content}</NewLineToBr>
-    </Container>
-  );
-}
-
-const MessageForm = ({
-  initText = '',
-  channelId,
-  messageId,
-  messageVersion,
-  activeMessage,
-  setActiveMessage,
-}) => {
-  const [post, setPost] = useState(initText);
-  const [alertVisible, setAlertVisible] = useState(false);
-
-  const submitHandler = (event) => {
-    event.preventDefault();
-    if (post.replace(/\s/g,'').length > 0) {
-      if (activeMessage && activeMessage.type === "edit") {
-        editMessageApi(messageId, messageVersion, post);
-        setActiveMessage(null);
-      }
-      else {
-        createMessageApi(post, channelId);
-        setPost("");
-      }
-    }
-    else {
-      setAlertVisible(true);
-    }
-  };
-  const cancelHandler = () => {
-    activeMessage && activeMessage.type === "edit" ? setActiveMessage(null) : setPost("")
-  }
-
-  return (
-      <form onSubmit={submitHandler}>
-      <Form>
-        <Container
-          header={
-            <Header
-              actions={
-                <SpaceBetween direction="horizontal" size="xs">
-                  <Button formAction="none" iconName="undo" variant="icon" onClick={cancelHandler} />
-                  <Button formAction="submit" iconName="upload" variant="icon" />
-                </SpaceBetween>
-              }
-            />
-          }
-        >
-          <Textarea
-            onChange={({detail}) => setPost(detail.value)}
-            value={post}
-            rows={post.split(/\r\n|\r|\n/).length}
-          />
-          <Modal
-            onDismiss={() => setAlertVisible(false)}
-            visible={alertVisible}
-            closeAriaLabel="Close modal"
-            size="small"
-          >
-            Please make sure to enter a message
-          </Modal>
-        </Container>
-      </Form>
-      </form>
-  );
-}
 
 export function Messages(channel) {
   const [messages, setMessages] = useState([]);
@@ -238,7 +62,7 @@ export function Messages(channel) {
   }, [channel.channelId])
 
   return (
-    <Grid gridDefinition={[{ colspan: 12 }, { colspan: 12 }]}>
+    <>
       <Box float='center' variant='h3'>Channel name</Box>
       <Box float='center'>
         <SpaceBetween size="xs">
@@ -255,6 +79,173 @@ export function Messages(channel) {
         <MessageForm channelId={channel.channelId} />
         </SpaceBetween>
       </Box>
-    </Grid>
+    </>
   );
+}
+
+function NewLineToBr({children = ""}) {
+  return children.split('\n').reduce(function (arr, line) {
+    return arr.concat(line, <br />);
+  },[]);
+}
+
+const NoMessage = () => {
+  return (
+    <SpaceBetween size="l">
+      <Box
+        margin={{ vertical: 'xs' }}
+        fontSize="heading-s"
+        textAlign="center"
+        color="inherit"
+      >
+        Please leave a message
+      </Box>
+    </SpaceBetween>
+  );
+}
+
+const Message = ({
+  message,
+  activeMessage,
+  setActiveMessage,
+}) => {
+  const [confirmVisible, setConfirmVisible] = useState(false);
+  const isEditing = activeMessage && activeMessage.type === "edit" && activeMessage.id === message.id
+  const deleteHandler = () => {
+    deleteMessageApi(message.id, message._version);
+    setConfirmVisible(false);
+  }
+
+  return (
+    isEditing ?
+    <MessageForm
+      initText={message.content}
+      channelId={message.channelId}
+      messageId={message.id}
+      messageVersion={message._version}
+      activeMessage={activeMessage}
+      setActiveMessage={setActiveMessage}
+     /> : <>
+    <Box variant="h4">User name</Box>
+    <Box color="text-body-secondary">{moment(message.updatedAt).fromNow()}</Box>
+    <Box>
+      <SpaceBetween direction="horizontal" size="xxs">
+        <Button iconName="contact" variant="icon" />
+        <Button iconName="edit" variant="icon" onClick={() => setActiveMessage({ id: message.id, type: "edit"})} />
+        <Button iconName="remove" variant="icon" onClick={() => setConfirmVisible(true)} />
+      </SpaceBetween>
+    </Box>
+    <Box>
+      <NewLineToBr>{message.content}</NewLineToBr>
+    </Box>
+    <Modal
+      onDismiss={() => setConfirmVisible(false)}
+      visible={confirmVisible}
+      closeAriaLabel="Close modal"
+      size="small"
+      footer={
+        <Box float="right">
+          <SpaceBetween direction="horizontal" size="xs">
+            <Button variant="link" onClick={() => setConfirmVisible(false)}>Cancel</Button>
+            <Button variant="primary" onClick={deleteHandler}>OK</Button>
+          </SpaceBetween>
+        </Box>
+      }
+    >
+      Are you sure to delete the message?
+    </Modal>
+    </>
+  );
+}
+
+const MessageForm = ({
+  initText = '',
+  channelId,
+  messageId,
+  messageVersion,
+  activeMessage,
+  setActiveMessage,
+}) => {
+  const [post, setPost] = useState(initText);
+  const [alertVisible, setAlertVisible] = useState(false);
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    if (post.replace(/\s/g,'').length > 0) {
+      if (activeMessage && activeMessage.type === "edit") {
+        editMessageApi(messageId, messageVersion, post);
+        setActiveMessage(null);
+      }
+      else {
+        createMessageApi(post, channelId);
+        setPost("");
+      }
+    }
+    else {
+      setAlertVisible(true);
+    }
+  };
+  const cancelHandler = () => {
+    activeMessage && activeMessage.type === "edit" ? setActiveMessage(null) : setPost("")
+  }
+
+  return (
+      <form onSubmit={submitHandler}>
+      <Form>
+        <Box>
+          <SpaceBetween direction="horizontal" size="xs">
+            <Button formAction="none" iconName="undo" variant="icon" onClick={cancelHandler} />
+            <Button formAction="submit" iconName="upload" variant="icon" />
+          </SpaceBetween>
+        </Box>
+        <Textarea
+          onChange={({detail}) => setPost(detail.value)}
+          value={post}
+          rows={post.split(/\r\n|\r|\n/).length}
+        />
+        <Modal
+          onDismiss={() => setAlertVisible(false)}
+          visible={alertVisible}
+          closeAriaLabel="Close modal"
+          size="small"
+        >
+          Please make sure to enter a message
+        </Modal>
+      </Form>
+      </form>
+  );
+}
+
+// apis
+function createMessageApi(post, channelId) {
+  try {
+    API.graphql(graphqlOperation(createMessage, {
+      input: { content: post, channelId: channelId }
+    }));
+  }
+  catch (e) {
+    console.log({e});
+  }
+}
+
+function editMessageApi(messageId, messageVersion, post) {
+  try {
+    API.graphql(graphqlOperation(updateMessage, {
+      input: { id: messageId, content: post, _version: messageVersion }
+    }));
+  }
+  catch (e) {
+    console.log({e});
+  }
+}
+
+function deleteMessageApi(messageId, messageVersion) {
+  try {
+    API.graphql(graphqlOperation(deleteMessage, {
+      input: { id: messageId, _version: messageVersion }
+    }));
+  }
+  catch (e) {
+    console.log({e});
+  }
 }
