0f2ace47f6a83a84ef86a161cafd9b80fc322152
Young-ook Kim <youngook.kim@gmail.com>
2023-06-13 11:14:19 +0900

    feat: workspace graphql api

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 // ui
-import React, { useRef } from "react";
+import React, { useState, useRef } from "react";
 import { AppLayout } from "@cloudscape-design/components";
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
 // components
-import { NavigationBar, Workspace } from "./components/Navigation";
+import { NavigationBar } from "./components/Navigation";
+import { Workspace } from "./components/Workspace";
 import { Channels } from "./components/Channel";
 
 // application
 function App ({ signOut, user }) {
+  const [activeWorkspace, setActiveWorkspace] = useState(null);
   const appLayout = useRef();
+
   return (
     <>
       <div id="h" style={{ position: 'sticky', top: 0, zIndex: 1002 }}>
@@ -19,8 +22,8 @@ function App ({ signOut, user }) {
       <AppLayout
         ref={appLayout}
         headerSelector="#h"
-        navigation={<Workspace />}
-        content={<Channels />}
+        navigation={<Workspace activeWorkspace={setActiveWorkspace} />}
+        content={<Channels workspaceId={activeWorkspace} />}
       />
     </>
   );
