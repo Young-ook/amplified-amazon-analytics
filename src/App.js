e86663f634dc994eafc9a6b7938a1d8df3ae8c96
Young-ook Kim <youngook.kim@gmail.com>
2023-07-11 22:23:27 +0900

    feat: flashbar notification

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import {
   AppLayout,
   Box,
   ContentLayout,
+  Flashbar,
   Header,
+  SpaceBetween
 } from "@cloudscape-design/components";
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
@@ -31,6 +33,26 @@ function App ({ signOut, user }) {
     });
   }, [context, loading, user]);
 
+  const [alerts, setAlerts] = useState([
+    {
+      type: "error",
+      dismissible: true,
+      dismissLabel: "Dismiss message",
+      header: "Failed to update instance id-4890f893e",
+      content: "This is a dismissible error message",
+      id: "message_3",
+      onDismiss: () => setAlerts(items => items.filter(item => item.id !== "message_3"))
+    },
+    {
+      type: "warning",
+      dismissible: true,
+      dismissLabel: "Dismiss message",
+      content: "This is a warning flash message",
+      id: "message_4",
+      onDismiss: () => setAlerts(items => items.filter(item => item.id !== "message_4"))
+    },
+  ]);
+
   return (
     <Box>
       <NavigationBar />
@@ -42,8 +64,33 @@ function App ({ signOut, user }) {
           loading ? (
             <Box/>
           ) : (
-            <ContentLayout header={<Header variant="h1" />}>
-              <Channels userId={user.attributes.sub} context={context} setContext={setContext} />
+            <ContentLayout
+              header={
+                <SpaceBetween size="m">
+                  <Header variant="h1" />
+                  <Flashbar
+                    items={alerts}
+                    i18nStrings={{
+                      ariaLabel: "Notifications",
+                      notificationBarAriaLabel: "View all notifications",
+                      notificationBarText: "Notifications",
+                      errorIconAriaLabel: "Error",
+                      warningIconAriaLabel: "Warning",
+                      successIconAriaLabel: "Success",
+                      infoIconAriaLabel: "Info",
+                      inProgressIconAriaLabel: "In progress"
+                    }}
+                    stackItems
+                  />
+                </SpaceBetween>
+              }
+            >
+              <Channels
+                userId={user.attributes.sub}
+                context={context}
+                setContext={setContext}
+                setAlerts={setAlerts}
+              />
             </ContentLayout>
           )
         }
